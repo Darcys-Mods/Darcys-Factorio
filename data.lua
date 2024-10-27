358c0600db5e5e6fb73eade8a01106fb6494295b
DarcyManoel <darcman00@gmail.com>
2024-10-28 09:24:05 +1030

    Un-cap player reach, kind of

--- data.lua
+++ data.lua
@@ -1,3 +1,9 @@
+local UINT32_MAX=(2^32)-1--maximum value for an unsigned 32-bit integer
+--	extend player reach
+data.raw['character']['character'].build_distance=UINT32_MAX
+data.raw['character']['character'].drop_item_distance=UINT32_MAX
+data.raw['character']['character'].reach_distance=UINT32_MAX
+data.raw['character']['character'].reach_resource_distance=UINT32_MAX
 --	tint entity ghosts to a vibrant teal
 local ghost_tint={
 	g=1,
