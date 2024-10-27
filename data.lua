5e74f2cb4815911660a52cf2edd7f7838ca8a862
DarcyManoel <darcman00@gmail.com>
2024-10-28 08:57:23 +1030

    Tint entity ghosts a vibrant teal

---
+++ data.lua
@@ -0,0 +1,11 @@
+--	tint entity ghosts to a vibrant teal
+local ghost_tint={
+	g=1,
+	b=1,
+	a=.3}
+local tile_ghost_tint={
+	g=1,
+	b=1,
+	a=.4}
+data.raw['utility-constants']['default'].ghost_tint=ghost_tint
+data.raw['utility-constants']['default'].tile_ghost_tint=tile_ghost_tint
