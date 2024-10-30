5e82271c88d8b9e333b58a0d6c84a720bf0f3315
DarcyManoel <darcman00@gmail.com>
2024-10-31 00:31:10 +1030

    Refactor multiple-use pointers

--- data.lua
+++ data.lua
@@ -1,13 +1,13 @@
 local UINT32_MAX=(2^32)-1--maximum value for an unsigned 32-bit integer
+local CHARACTER=data.raw['character']['character']
+local DEFAULT_UTILITY_CONSTANTS=data.raw['utility-constants']['default']
 --	remove character hit-box (no clip)
-data.raw['character']['character'].collision_box={
-	{0,0},
-	{0,0}}
+CHARACTER.collision_box={{0,0},{0,0}}
 --	extend player reach
-data.raw['character']['character'].build_distance=UINT32_MAX
-data.raw['character']['character'].drop_item_distance=UINT32_MAX
-data.raw['character']['character'].reach_distance=UINT32_MAX
-data.raw['character']['character'].reach_resource_distance=UINT32_MAX
+CHARACTER.build_distance=UINT32_MAX
+CHARACTER.drop_item_distance=UINT32_MAX
+CHARACTER.reach_distance=UINT32_MAX
+CHARACTER.reach_resource_distance=UINT32_MAX
 --	tint entity ghosts to a vibrant teal
 local ghost_tint={
 	g=1,
@@ -17,5 +17,5 @@ local tile_ghost_tint={
 	g=1,
 	b=1,
 	a=.4}
-data.raw['utility-constants']['default'].ghost_tint=ghost_tint
-data.raw['utility-constants']['default'].tile_ghost_tint=tile_ghost_tint
+DEFAULT_UTILITY_CONSTANTS.ghost_tint=ghost_tint
+DEFAULT_UTILITY_CONSTANTS.tile_ghost_tint=tile_ghost_tint
